3e69ae0435cdb72bd7f81e01b45864b6bf9a9565
Mirza Anzar Baig <mabaig222@gmail.com>
2023-05-28 23:19:53 +0530

    second

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { getServerSession } from 'next-auth'
 import Image from 'next/image'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
-
+// hey 
 const page = async ({}) => {
   const session = await getServerSession(authOptions)
   if (!session) notFound()
